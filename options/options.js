16866754223c99452b7f4b24e4d7348888d23446
Crowdbotics <team@crowdbotics.com>
2024-01-23 00:17:04 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "purple-bird-45477",
+  url: "https://purple-bird-45477.botics.co",
+  api: "https://purple-bird-45477.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
